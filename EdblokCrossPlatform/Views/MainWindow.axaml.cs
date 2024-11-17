70641351548a2591f5f0f2fc488ec5165daebd25
Artem <Artem@DESKTOP-4BOPE1O>
2024-11-17 13:26:17 +0300

    Init

---
+++ EdblokCrossPlatform/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace EdblokCrossPlatform.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
